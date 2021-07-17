c915f4215d4b543bb7c6ac096677801974c61cb9
Ying Wen <ying.wen@sjtu.edu.cn>
2021-07-17 19:33:59 +0800

    fixed eval; fixed bean gen

--- run_log.py
+++ run_log.py
@@ -6,6 +6,21 @@ from env.chooseenv import make
 from util.get_logger import get_logger
 from env.obs_interfaces.observation import obs_type
 import argparse
+import numpy as np
+from copy import deepcopy
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)
+
+
 
 def get_players_and_action_space_list(g):
     if sum(g.agent_nums) != g.n_player:
@@ -54,7 +69,9 @@ def get_joint_action_eval(game, player_ids, policy_list, actions_spaces, info_be
 
         action_space_list = actions_spaces[policy_i]
         function_name = 'm%d' % policy_i
-        each = eval(function_name)(obs_list, action_space_list, game.is_act_continuous)
+        obs_list_togo = deepcopy(obs_list)
+        each = eval(function_name)(obs_list_togo, action_space_list, game.is_act_continuous)
+
         if len(each) != game.agent_nums[policy_i]:
             error = "模型动作空间维度%d不正确！应该是%d" % (len(each), game.agent_nums[policy_i])
             raise Exception(error)
@@ -119,7 +136,7 @@ def run_game(g, env_name, player_ids, actions_spaces, policy_list):
         game_info["n_return"] = g.n_return
         ed = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
         game_info["end_time"] = ed
-        logs = json.dumps(game_info, ensure_ascii=False)
+        logs = json.dumps(game_info, ensure_ascii=False, cls=NpEncoder)
         logger.info(logs)
 
 
@@ -135,7 +152,7 @@ if __name__ == "__main__":
     render_mode = True
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--my_ai", default="random", help="dqn/random/greedy")
+    parser.add_argument("--my_ai", default="greedy", help="dqn/random/greedy")
     parser.add_argument("--opponent", default="greedy", help="dqn/random/greedy")
     args = parser.parse_args()
 
